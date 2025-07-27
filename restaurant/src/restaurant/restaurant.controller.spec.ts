f13169c51d3d0f479981e0dc6b025d672e94fd29
oskar0kh <oskar0kh@g.skku.edu>
2025-07-27 18:30:22 +0900

    GET Request: Service, Controller spec.ts 테스트 코드 작성 완료

--- restaurant/src/restaurant/restaurant.controller.spec.ts
+++ restaurant/src/restaurant/restaurant.controller.spec.ts
@@ -25,7 +25,7 @@ describe('RestaurantController', () => { // 1-1. 현재 테스트 그룹명 = 'R
   });
 });
 
-// 2. Controller의 'getAllRestaurants()' 메서드 테스트
+// 2. Controller - 'getAllRestaurants()' 테스트
 describe('RestaurantController - getAllRestaurants() 통합 테스트', () => {
   let controller: RestaurantController;
   let service: RestaurantService;
@@ -34,12 +34,12 @@ describe('RestaurantController - getAllRestaurants() 통합 테스트', () => {
   const testFilePath = path.join(__dirname, 'test-data', 'restaurants.test.json');
 
   // 2-2. parsing한 test용 JSON 데이터를 저장하는 변수 (메서드 실행 결과랑 비교하기 위함)
-  let testData: RestaurantList;
+  let testJsonData: RestaurantList;
 
   // 2-3. test용 JSON 데이터 먼저 읽기 (메서드 실행 결과랑 비교하기 위함)
   beforeAll(async () => {
     const raw = await fs.promises.readFile(testFilePath, 'utf-8'); // test용 JSON 데이터 읽기
-    testData = JSON.parse(raw); // testData 변수에, JSON 데이터 저장
+    testJsonData = JSON.parse(raw); // testJsonData 변수에, JSON 데이터 저장
   });
 
   // 2-4. test용 모듈 생성, 초기 환경 설정
@@ -62,12 +62,67 @@ describe('RestaurantController - getAllRestaurants() 통합 테스트', () => {
       json: jest.fn(), // json(data) 역할 : 자기가 가지고 있는 데이터 그대로 출력
     };
 
-    await controller.getAllRestaurants(mockRes as any); // 실제 Controller의 'getAllRestaurants()' 메서드 실행
+    // 1) 실제 Controller의 'getAllRestaurants()' 메서드 실행
+    await controller.getAllRestaurants(mockRes as any);
 
-    // 반환된 status 코드 비교하기 (ex: 데이터 직접 읽었을 때의 200 코드 == getAllRestaurants의 200 코드)
+    // 2) 반환된 status 코드 비교하기 (ex: 데이터 직접 읽었을 때의 200 코드 == getAllRestaurants의 200 코드)
     expect(mockRes.status).toHaveBeenCalledWith(200);
 
-    // 직접 읽었을 때의 JSON 데이터 == getAllRestaurants() 메서드의 JSON 데이터 결과값 비교
-    expect(mockRes.json).toHaveBeenCalledWith(testData);
+    // 3) 직접 읽었을 때의 JSON 데이터 == getAllRestaurants() 메서드의 JSON 데이터 결과값 비교
+    expect(mockRes.json).toHaveBeenCalledWith(testJsonData);
+  });
+});
+
+// 3. Controller - 'getRestaurantByName()' 테스트
+describe('RestaurantController - getRestaurantByName() 통합 테스트', () => {
+  let controller: RestaurantController;
+  let service: RestaurantService;
+
+  // 3-1. test용 경로 선언
+  const testFilePath = path.join(__dirname, 'test-data', 'restaurants.test.json');
+
+  // 3-2. test용 JSON 저장할 변수 선언
+  let testJsonData: RestaurantList;
+
+  // 3-3. test용 JSON 데이터 전체 가져오기 (메서드 실행 결과랑 비교하기 위함)
+  beforeAll(async () => {
+    const raw = await fs.promises.readFile(testFilePath, 'utf-8');
+    testJsonData = JSON.parse(raw);
+  });
+
+  // 3-4. test용 모듈 생성, 초기 환경 설정
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [RestaurantController],    // TestingModule에 어떤 Controller 넣을지 지정
+      providers: [RestaurantService],         // TestingModule에 어떤 Service 넣을지 지정
+    }).compile();
+
+    controller = module.get<RestaurantController>(RestaurantController); // controller 모듈 생성
+    service = module.get<RestaurantService>(RestaurantService);          // service 모듈 생성
+
+    (service as any).filePath = testFilePath; // 테스트용 JSON 파일을 사용하도록, service의 filePath 수정
+  });
+
+  // 3-5. 실제 테스트 케이스 로직 작성
+  it('Test용 JSON 파일에서 특정 name에 해당하는 데이터 가져오는지 확인 (Controller)', async () => {
+    const mockRes = { // mockRes : Express의 Response 객체를 Mocking
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    // 1) name 1개 지정
+    const name: string = '먹거리 고을';
+
+    // 2) 실제 Controller의 메서드 실행 (name도 건네주기)
+    await controller.getRestaurantByName(mockRes as any, name);
+
+    // 3) test용 JSON 데이터에서, name이랑 같은 데이터 찾기
+    const expected = testJsonData.restaurants.find(r => r.name === name);
+
+    // 4) 반환된 status 코드 비교하기
+    expect(mockRes.status).toHaveBeenCalledWith(200);
+
+    // 5) 'getRestaurantByName 메서드의 결과값 == test용 JSON 데이터에서 찾은 데이터' 인지 확인
+    expect(mockRes.json).toHaveBeenCalledWith(expected);
   });
 });
--- restaurant/src/restaurant/restaurant.service.spec.ts
+++ restaurant/src/restaurant/restaurant.service.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { RestaurantService } from './restaurant.service';
 import { Res } from '@nestjs/common';
 
+import { RestaurantList } from './interface/restaurant.interface';
 const fs = require('fs');
 const path = require('path');
 
@@ -22,7 +23,7 @@ describe('RestaurantService', () => {
   });
 });
 
-// 1. getAllRestaurants() 메서드 테스트
+// 1. Service - 'getAllRestaurants()' 테스트
 describe('RestaurantService - getAllRestaurants() 유닛 테스트', () => {
   let service: RestaurantService;
 
@@ -50,5 +51,40 @@ describe('RestaurantService - getAllRestaurants() 유닛 테스트', () => {
 
     // 2개 결과가 같은지 비교
     expect(result).toEqual(expectedData);
+  });
+});
+
+// 2. Service - 'getRestaurantByName()' 테스트
+describe('RestaurantService - getRestaurantByName() 유닛 테스트', () => {
+  let service: RestaurantService;
+
+  const testFilePath = path.join(__dirname, 'test-data', 'restaurants.test.json');
+
+  let testJsonData: RestaurantList; // 전체 JSON 데이터 저장하는 전역 변수 선언
+
+  // 2-1. 먼저 test용 JSON 데이터 전체 가져오기
+  beforeAll(async () => {
+    const raw = await fs.promises.readFile(testFilePath, 'utf-8'); // test용 JSON 데이터 읽기
+    testJsonData = JSON.parse(raw); // jsonData 변수에, JSON 데이터 저장
+  });
+
+  beforeEach(() => {
+    service = new RestaurantService();
+
+    (service as any).filePath = testFilePath; // test용 JSON 파일 경로로 수정
+  });
+
+  it('Test용 JSON 파일에서 특정 name에 해당하는 데이터 가져오는지 확인 (Service)', async() => {
+    // 2-2. test용 name 하나 생성
+    const targetName: string = '먹거리 고을';
+
+    // 2-3. 전체 test용 JSON 데이터에서, name이랑 같은 데이터 찾기
+    const expected = testJsonData.restaurants.find(r => r.name === targetName);
+    
+    // 2-4. 실제 getRestaurantByName 메서드 실행
+    const result = await service.getRestaurantByName(targetName);
+
+    // 2-4. 예상값 == 실제값 비교
+    expect(result).toEqual(expected);
   });
 });
