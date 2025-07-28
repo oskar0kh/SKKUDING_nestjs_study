56080857ca3090132599fcdefaae2a9e3b4030de
oskar0kh <oskar0kh@g.skku.edu>
2025-07-28 17:57:05 +0900

    Service의 유닛 테스트, Controller와의 통합 테스트 전부 구현 완료

--- restaurant/src/restaurant/restaurant.controller.spec.ts
+++ restaurant/src/restaurant/restaurant.controller.spec.ts
@@ -2,8 +2,9 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { RestaurantController } from './restaurant.controller';
 
 import { RestaurantService } from './restaurant.service'; // RestaurantService 모듈 가져오기
-import { RestaurantList } from './interface/restaurant.interface'; // RestaurantList 타입 가져오기
+import { RestaurantList, Restaurant } from './interface/restaurant.interface'; // RestaurantList 타입 가져오기
 import { REPLCommand } from 'repl';
+
 const fs = require('fs');
 const path = require('path');
 
@@ -124,5 +125,196 @@ describe('RestaurantController - getRestaurantByName() 통합 테스트', () => 
 
     // 5) 'getRestaurantByName 메서드의 결과값 == test용 JSON 데이터에서 찾은 데이터' 인지 확인
     expect(mockRes.json).toHaveBeenCalledWith(expected);
+  });
+});
+
+// 4. Controller - 'addRestaurant()' 테스트
+describe('RestaurantController - addRestaurant() 통합 테스트', () => {
+  let controller: RestaurantController;
+  let service: RestaurantService;
+
+  // 4-1. test용 경로, test용 JSON 데이터 저장할 변수 선언
+  const testFilePath = path.join(__dirname, 'test-data', 'restaurants.test.json');
+
+  let testJsonData1: RestaurantList; // 직접 데이터 집어넣을 JSON 변수
+  let testJsonData2: RestaurantList; // Service 로직으로 데이터 집어넣을 변수
+
+  // 4-2. test용 JSON 데이터 전체 가져오기 (testJsonData1에 저장)
+  beforeAll(async () => {
+    const raw = await fs.promises.readFile(testFilePath, 'utf-8');
+    testJsonData1 = JSON.parse(raw);
+  });
+
+  // 4-3. test용 모듈 생성, 초기 환경 설정
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [RestaurantController],
+      providers: [RestaurantService],
+    }).compile();
+
+    controller = module.get<RestaurantController>(RestaurantController);
+    service = module.get<RestaurantService>(RestaurantService);
+
+    (service as any).filePath = testFilePath;
+  });
+
+  // 4-4. '직접 데이터 넣은 JSON 파일 == addRestaurant Service 로직 사용한 JSON 파일' 같은지 비굔
+  it('Test용 JSON 파일에 데이터 추가하는 기능 확인 (Controller)', async () => {
+    const mockRes = { // mockRes : Express의 Response 객체를 Mocking
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    // 1) 집어넣을 JSON 데이터 1개 만들기
+    const testData = {
+      "name": "알촌",
+      "address": "경기도 수원시 당구장쪽",
+      "phone": "1111-5555-9999"
+    };
+
+    // 2) 직접 JSON 파일에 데이터 넣기
+    testJsonData1.restaurants.push(testData);
+
+    // 3) controller 메서드 사용 -> JSON 파일에 데이터 넣기 -> testJsonData2로 변경된 파일 다시 읽어오기
+    await controller.addRestaurant(mockRes as any, testData);
+
+    const raw = await fs.promises.readFile(testFilePath, 'utf-8');
+    testJsonData2 = JSON.parse(raw); // 데이터 추가된 파일 다시 가져오기
+
+    // 4) 반환된 status 코드 비교하기 (POST 헨들러: 201 코드 반환)
+    expect(mockRes.status).toHaveBeenCalledWith(201);
+
+    // 5) 'addRestaurant 메서드의 결과 JSON 파일 == 직접 데이터 넣은 JSON 파일' 인지 확인
+    expect(testJsonData2).toEqual(testJsonData1);
+  });
+});
+
+// 5. Controller - 'deleteRestaurant()' 테스트
+describe('RestaurantController - deleteRestaurant() 통합 테스트', () => {
+  let controller: RestaurantController;
+  let service: RestaurantService;
+
+  const testFilePath = path.join(__dirname, 'test-data', 'restaurants.test.json');
+
+  let testJsonData: RestaurantList;
+
+  // 5-1. test용 JSON 데이터 전체 가져오기 (testJsonData에 저장)
+  beforeAll(async () => {
+    const raw = await fs.promises.readFile(testFilePath, 'utf-8');
+    testJsonData = JSON.parse(raw);
+  });
+
+  // 5-2. test용 모듈 생성, 초기 환경 설정
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [RestaurantController],
+      providers: [RestaurantService],
+    }).compile();
+
+    controller = module.get<RestaurantController>(RestaurantController);
+    service = module.get<RestaurantService>(RestaurantService);
+
+    (service as any).filePath = testFilePath;
+  });
+
+  // 5-3. '직접 삭제한 후의 반환값 == Controller로 삭제한 후의 반환값' 같은지 비교
+  it('Test용 JSON 파일에 특정 name의 데이터 삭제하는 기능 확인 (Controller)', async () => {
+    const mockRes = { // mockRes : Express의 Response 객체를 Mocking
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    // 1) 테스트용 name 1개 지정
+    const targetName: string = "알촌";
+
+    // 2) 직접 데이터를 JSON 파일에서 삭제 & 삭제한 데이터 반환 (testJsonData)
+
+      // 1.. targetName이랑 같은 이름의 데이터 인덱스 찾기
+    const index: number | undefined = testJsonData.restaurants.findIndex(r => r.name === targetName)
+
+      // 2.. testJsonData1 안에서 해당 데이터 잘라내기
+    const removedItem1: Restaurant = testJsonData.restaurants.splice(index, 1)[0];
+
+    // 3) Controller에서 deleteRestaurant() Service 로직 사용 -> 원본 파일에서 삭제 -> 삭제한 데이터 반환
+    //  -> controller의 deleteRestaurant()의 반환값 = void
+    //  ∴ 반환값 사용하려면, mockRes에서 값 추출해야함!
+    await controller.deleteRestaurant(mockRes as any, targetName);
+
+    // 4) 반환된 status 코드 비교
+    expect(mockRes.status).toHaveBeenCalledWith(200);
+
+    // 5) 2개 반환값 같은지 비교 (mockRes에서 controller 로직의 반환값 추출)
+    expect(mockRes.json).toHaveBeenCalledWith(removedItem1);
+  });
+});
+
+// 6. Controller - 'patchRestaurant()' 테스트
+describe('RestaurantController - patchRestaurant() 통합 테스트', () => {
+  let controller: RestaurantController;
+  let service: RestaurantService;
+
+  const testFilePath = path.join(__dirname, 'test-data', 'restaurants.test.json');
+
+  let testJsonData: RestaurantList;
+
+  // 6-1. test용 JSON 데이터 전체 가져오기 (testJsonData에 저장)
+  beforeAll(async () => {
+    const raw = await fs.promises.readFile(testFilePath, 'utf-8');
+    testJsonData = JSON.parse(raw);
+  });
+
+  // 6-2. test용 모듈 생성, 초기 환경 설정
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [RestaurantController],
+      providers: [RestaurantService],
+    }).compile();
+
+    controller = module.get<RestaurantController>(RestaurantController);
+    service = module.get<RestaurantService>(RestaurantService);
+
+    (service as any).filePath = testFilePath;
+  });
+
+  // 6-3. '직접 수정한 JSON 파일 == Controller로 수정한 원본 test용 JSON 파일' 같은지 비교
+  it('Test용 JSON 파일에 특정 name의 데이터 수정하는 기능 확인 (Controller)', async () => {
+    const mockRes = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    // 1) 수정할 JSON 데이터 1개 만들기
+    const testData = {
+      "name": "행컵",
+      "address": "경기도 수원시 당구장쪽",
+      "phone": "8282-5555-2323",
+      "rating": 4.1 // 추가된 값 (요걸로 수정할거임)
+    };
+
+    // 2) 직접 testData의 name과 같은 데이터 찾기 -> 해당 데이터의 정보 수정
+    const found: Restaurant | undefined = testJsonData.restaurants.find(r => r.name === testData.name);
+
+      // * found가 undefined인 경우, Error 반환하도록 설계
+    if(found) {
+      Object.assign(found, {
+        ...testData,
+        name: found.name  // 기존 name은 유지하고, 나머지 정보들만 testData속 정보들로 바꾸기
+      });
+    } else {
+      throw new Error(`Restaurant with name ${testData.name} not found`);
+    }
+
+    // 3) Controller의 patchRestaurant() 사용 : 원본 test용 JSON 파일 수정하기
+    await controller.patchRestaurant(mockRes as any, testData.name, testData);
+
+    // 4) 수정된 원본 JSON 파일 다시 읽어오기
+    const raw = await fs.promises.readFile(testFilePath, 'utf-8');
+    const updatedJson = JSON.parse(raw);
+
+    // 5) 반환된 status 코드 비교
+    expect(mockRes.status).toHaveBeenCalledWith(200);
+
+    // 6) '수정된 원본 JSON 파일 == testJsonData' 같은지 비교
+    expect(updatedJson).toEqual(testJsonData);
   });
 });
--- restaurant/src/restaurant/restaurant.service.spec.ts
+++ restaurant/src/restaurant/restaurant.service.spec.ts
@@ -2,7 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { RestaurantService } from './restaurant.service';
 import { Res } from '@nestjs/common';
 
-import { RestaurantList } from './interface/restaurant.interface';
+import { RestaurantList, Restaurant } from './interface/restaurant.interface';
 const fs = require('fs');
 const path = require('path');
 
@@ -86,5 +86,156 @@ describe('RestaurantService - getRestaurantByName() 유닛 테스트', () => {
 
     // 2-4. 예상값 == 실제값 비교
     expect(result).toEqual(expected);
+  });
+});
+
+// 3. Service - 'addRestaurant()' 테스트
+describe('RestaurantService - addRestaurant() 유닛 테스트', () => {
+  let service: RestaurantService;
+
+  // __dirname: 현재 service.spec.ts 파일이 위치한 곳
+  const testFilePath = path.join(__dirname, 'test-data', 'restaurants.test.json');
+  
+  // 3-1. 변수 2개 선언: 직접 데이터 넣을 JSON 파일 (testJsonData1), addRestaurant()로 넣을 JSON 파일 (testJsonData2)
+  let testJsonData1: RestaurantList;
+  let testJsonData2: RestaurantList;
+
+  // 3-2. 먼저 test용 JSON 데이터 전체 가져오기 -> testJsonData1에 저장
+  beforeAll(async () => {
+    const raw = await fs.promises.readFile(testFilePath, 'utf-8');
+    testJsonData1 = JSON.parse(raw);
+  });
+
+  // 3-3. 매 it 테스트마다 service 객체 새로 생성하고 테스트 -> 테스트간 독립성 보장
+  beforeEach(() => {
+    service = new RestaurantService();
+    (service as any).filePath = testFilePath;
+  });
+
+  // 3-4. '직접 넣은 데이터 == addRestaurant()로 넣은 데이터' 확인
+  //      -> 'testJsonData1 == testJsonData2' 둘이 같은지 확인
+  it('Test용 Json 파일에 새로운 restaurant 데이터 추가하는 기능 확인 (Service)', async() => {
+
+    // 1) 테스트용 데이터 1개 만들기
+    const testData = {
+      "name": "알촌",
+      "address": "경기도 수원시 당구장쪽",
+      "phone": "1111-5555-9999"
+    };
+
+    // 2) 직접 데이터를 JSON 파일에 넣기 (testJsonData1)
+    testJsonData1.restaurants.push(testData);
+
+    // 3) addRestaurant()로 데이터 넣기
+    await service.addRestaurant(testData); // addRestaurant()의 return값은 쓸 필요 없으므로, 아예 return값 안받기
+
+    // 4) addRestaurant() 실행 후, 다시 JSON 파일 읽기 (∵ addRestaurant로 JSON 파일이 바뀌었으니, 다시 읽어와야함)
+    const raw = await fs.promises.readFile(testFilePath, 'utf-8');
+    testJsonData2 = JSON.parse(raw); // 바뀐 JSON 파일 -> testJsonData2에 저장
+
+    // 5) 2개 JSON 파일 속 데이터가 일치하는지 확인
+    expect(testJsonData2).toEqual(testJsonData1);
+  });
+});
+
+// 4. Service - 'deleteRestaurant()' 테스트
+describe('RestaurantService - deleteRestaurant() 유닛 테스트', () => {
+  let service: RestaurantService;
+
+  const testFilePath = path.join(__dirname, 'test-data', 'restaurants.test.json');
+  
+  // 4-1. 직접 데이터 삭제할 JSON 파일의 변수 선언
+  let testJsonData: RestaurantList;
+
+  // 4-2. 먼저 test용 JSON 데이터 전체 가져오기
+  beforeAll(async () => {
+    const raw = await fs.promises.readFile(testFilePath, 'utf-8');
+    testJsonData = JSON.parse(raw);
+  });
+
+  // 4-3. 매 it 테스트마다 service 객체 새로 생성
+  beforeEach(() => {
+    service = new RestaurantService();
+    (service as any).filePath = testFilePath;
+  });
+
+  // 4-4. '직접 삭제한 후의 JSON 데이터 == deleteRestaurant()로 삭제한 후의 JSON 데이터' 확인
+  //      -> 'testJsonData에서 삭제한 후 꺼내온 데이터 == 원본 test용 JSON 파일에서 삭제한 후 꺼내온 데이터' 같은지 확인
+  //      -> 'removedItem1 == removedItem2' 인지 확인
+  it('Test용 Json 파일에 특정 name의 restaurant 데이터 삭제하는지 확인 (Service)', async() => {
+
+    // 1) 테스트용 name 1개 설정
+    const targetName: string = '알촌';
+
+    // 2) 직접 데이터를 JSON 파일에서 삭제 & 삭제한 데이터 저장 (testJsonData)
+
+      // 1.. targetName이랑 같은 이름의 데이터 인덱스 찾기
+    const index: number | undefined = testJsonData.restaurants.findIndex(r => r.name === targetName)
+
+      // 2.. testJsonData1 안에서 해당 데이터 잘라내기
+    const removedItem1: Restaurant = testJsonData.restaurants.splice(index, 1)[0];
+
+    // 3) deleteRestaurant() 사용 : 원본 test용 JSON 파일에서 해당 name의 데이터 삭제 & 삭제한 데이터 반환
+    const removedItem2: Restaurant = await service.deleteRestaurant(targetName);
+
+    // 4) 삭제한 후 반환한 2개 데이터가 일치하는지 확인
+    expect(removedItem2).toEqual(removedItem1);
+  });
+});
+
+// 5. Service - 'patchRestaurant()' 테스트
+describe('RestaurantService - patchRestaurant() 유닛 테스트', () => {
+  let service: RestaurantService;
+
+  const testFilePath = path.join(__dirname, 'test-data', 'restaurants.test.json');
+
+  // 5-1. 직접 데이터 수정할 JSON 파일의 변수 선언
+  let testJsonData: RestaurantList;
+
+  // 5-2. 먼저 test용 JSON 데이터 전체 가져오기
+  beforeAll(async () => {
+    const raw = await fs.promises.readFile(testFilePath, 'utf-8');
+    testJsonData = JSON.parse(raw);
+  });
+
+  // 5-3. 매 it 테스트마다 service 객체 새로 생성
+  beforeEach(() => {
+    service = new RestaurantService();
+    (service as any).filePath = testFilePath;
+  });
+
+  // 5-4. '직접 수정한 JSON 파일 == patchRestaurant()로 수정한 원본 test용 JSON 파일' 같은지 비교
+  it('Test용 Json 파일에 특정 name의 restaurant 데이터 수정하는지 확인 (Service)', async() => {
+    
+    // 1) 수정할 JSON 데이터 1개 만들기
+    const testData = {
+      "name": "성대 밥상",
+      "address": "경기도 수원시 당구장쪽",
+      "phone": "1111-5555-9999",
+      "rating": 4.0 // 추가된 값 (요걸로 수정할거임)
+    };
+
+    // 2) 직접 testData의 name과 같은 데이터 찾기 -> 해당 데이터의 정보 수정
+    const found: Restaurant | undefined = testJsonData.restaurants.find(r => r.name === testData.name);
+
+      // * found가 undefined인 경우, Error 반환하도록 설계
+    if(found) {
+      Object.assign(found, {
+        ...testData,
+        name: found.name  // 기존 name은 유지하고, 나머지 정보들만 testData속 정보들로 바꾸기
+      });
+    } else {
+      throw new Error(`Restaurant with name ${testData.name} not found`);
+    }
+
+    // 3) patchRestaurant() 사용 : 원본 test용 JSON 파일 수정하기
+    await service.patchRestaurant(testData.name, testData);
+
+    // 4) 수정된 원본 JSON 파일 다시 읽어오기
+    const raw = await fs.promises.readFile(testFilePath, 'utf-8');
+    const updatedJson = JSON.parse(raw);
+
+    // 5) '수정된 원본 JSON 파일 == testJsonData' 같은지 비교
+    expect(updatedJson).toEqual(testJsonData);
   });
 });
